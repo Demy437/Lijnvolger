ba8b8a085ba1d5083cf82d028fffa0230fb4b354
Gideon Feenstra <gideon.feenstra@outlook.com>
2023-03-09 11:40:07 +0100

    0-1 voor sensoren

--- rijdtest/rijdtest.ino
+++ rijdtest/rijdtest.ino
@@ -56,6 +56,14 @@ if (value_3 == LOW){ //T Point
 
 }
 // Function to go forward
+String readSensors(){
+  String value_1 = String(digitalRead(s1));
+  String value_2 = String(digitalRead(s2));
+  String value_3 = String(digitalRead(s3));
+  String value_4 = String(digitalRead(s4));
+  String value_5 = String(digitalRead(s5));
+  return value_1 + value_2 + value_3 + value_4 + value_5;
+}
 void forward() {
   analogWrite(pwmPinR, 100);
   digitalWrite(directionPinR, LOW);
